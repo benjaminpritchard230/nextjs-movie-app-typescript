923b7bf63026d89fdb390cc092fe31554ce7564c
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-02-02 19:59:51 +0000

    Add redirect from /movies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = [{
   reactStrictMode: true,
-}
+},{
+  async redirects() {
+    return [
+      {
+        source: "/movies",
+        destination: "/movies/popular",
+        permanent: true,
+      },
+    ];
+  },
+};]
 
 module.exports = nextConfig
