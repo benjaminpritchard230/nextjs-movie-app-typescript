e987d55a3ccd371d9c4c5c220a0987ea8613edef
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-02-11 16:35:52 +0000

    Add more data to people info page

--- pages/people/[peopleId].tsx
+++ pages/people/[peopleId].tsx
@@ -12,6 +12,7 @@ import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import { CLIENT_RENEG_LIMIT } from "tls";
 
 type Props = {
@@ -25,6 +26,8 @@ const PeopleDetail = ({ data }: Props) => {
     return `https://www.themoviedb.org/t/p/w1280/${data.profile_path}`;
   };
   console.log(data);
+
+  const percentage = Math.floor(data.popularity);
 
   const MyImage = () => {
     return (
@@ -44,7 +47,26 @@ const PeopleDetail = ({ data }: Props) => {
     separator: string = " "
   ): string => {
     if (str.length <= maxLen) return str;
-    return str.substr(0, str.lastIndexOf(separator, maxLen));
+    return str.substring(0, str.lastIndexOf(separator, maxLen));
+  };
+
+  const getAge = (birthday: string) => {
+    const today = new Date();
+    const birthDate = new Date(birthday);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  const getAgeAtDeath = (birthday: string, deathday: string) => {
+    const birthDate = new Date(birthday);
+    const deathDate = new Date(deathday);
+    const age = deathDate.getFullYear() - birthDate.getFullYear();
+
+    return age;
   };
 
   return (
@@ -57,25 +79,34 @@ const PeopleDetail = ({ data }: Props) => {
         <div className={styles.item}>
           <ul>
             <li>
-              <p>Date of birth: {data.birthday}</p>
+              <h5>Date of birth: {data.birthday}</h5>
               {data.deathday ? (
-                <p>{`Date of death: ${data.deathday}`}</p>
+                <h5>{`Date of death: ${data.deathday}`}</h5>
               ) : null}
-              <p>Birthplace: {data.place_of_birth}</p>
-              <p>Gender: {data.gender === 1 ? "female" : "male"}</p>
-              <p>Also known as: {data.also_known_as.join(", ")}</p>
-              <p>
+              {data.deathday ? (
+                <h5>
+                  Age at death: {getAgeAtDeath(data.birthday, data.deathday)}
+                </h5>
+              ) : (
+                <h5>Age: {getAge(data.birthday)}</h5>
+              )}
+              <h5>Birthplace: {data.place_of_birth}</h5>
+              <h5>Gender: {data.gender === 1 ? "female" : "male"}</h5>
+              <h5>Also known as: {data.also_known_as.join(", ")}</h5>
+              <h5>
                 <a href={`https://www.imdb.com/name/${data.imdb_id}/`}>
                   Link to IMDb profile
                 </a>
-              </p>
+              </h5>
             </li>
           </ul>
         </div>
         <div className={styles.item}>
           <p>{shorten(data.biography, 600)}</p>
         </div>
-        <div className={styles.item}></div>
+        <div className={styles.item}>
+          <h5>Popularity rating: {data.popularity}</h5>
+        </div>
       </div>
     </>
   );
