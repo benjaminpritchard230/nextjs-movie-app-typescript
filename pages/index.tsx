61e79cb5a642ddd91ed8fc3522561710dd3fddcd
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-02-23 21:14:51 +0000

    Add homepage using css grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,34 @@
 import Layout from "@/components/Layout";
+import styles from "@/styles/HomePage.module.css";
 import { dividerClasses } from "@mui/material";
 import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 
-const Page: NextPageWithLayout = () => {
+const HomePage = () => {
   return (
-    <div>
-      <button className="btn btn-primary">hello</button>
-    </div>
+    <>
+      <div className={styles["parent"]}>
+        <div className={styles["item1"]}>
+          <div className={styles["search"]}>
+            <h2>Search now for movies, TV shows or people...</h2>
+            <div className={styles["input-container"]}>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles["item2"]}>
+          <h2>Popular movies</h2>
+        </div>
+        <div className={styles["item3"]}>
+          <h2>Popular TV shows</h2>
+        </div>
+        <div className={styles["item4"]}>
+          <h2>Popular people</h2>
+        </div>
+      </div>
+    </>
   );
 };
 
-Page.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
-};
-
-export default Page;
+export default HomePage;
