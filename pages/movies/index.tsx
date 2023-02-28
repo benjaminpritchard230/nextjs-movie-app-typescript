ffa48c9a01843553d87cbedf3380f1a5e8bfe352
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-02-28 16:14:31 +0000

    Add homepages for movies and tv shows

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import { useSearchText } from "@/context/SearchTextContext";
+import styles from "@/styles/MovieHome.module.css";
 import React from "react";
 
 type Props = {};
@@ -9,6 +10,32 @@ const MovieHome = (props: Props) => {
   return (
     <>
       <Header text="Movies home" />
+      <div className={styles["parent"]}>
+        <div className={styles["item0"]}>
+          <div className={styles["search"]}>
+            <h2>Search now for movies, TV shows or people...</h2>
+            <div className={styles["input-container"]}>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles["item1"]}>
+          <h2>Bullet points</h2>
+        </div>
+        <div className={styles["item2"]}>
+          <h2>Popular</h2>
+        </div>
+        <div className={styles["item3"]}>
+          <h2>Now playing</h2>
+        </div>
+        <div className={styles["item4"]}>
+          <h2>Upcoming</h2>
+        </div>
+        <div className={styles["item5"]}>
+          <h2>Top rated</h2>
+        </div>
+      </div>
     </>
   );
 };
--- pages/tv-shows/index.tsx
+++ pages/tv-shows/index.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import styles from "@/styles/TvHome.module.css";
 import React from "react";
 
 type Props = {};
@@ -7,6 +8,32 @@ const TvShowHome = (props: Props) => {
   return (
     <>
       <Header text="TV Shows home" />
+      <div className={styles["parent"]}>
+        <div className={styles["item0"]}>
+          <div className={styles["search"]}>
+            <h2>Search now for movies, TV shows or people...</h2>
+            <div className={styles["input-container"]}>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles["item1"]}>
+          <h2>Bullet points</h2>
+        </div>
+        <div className={styles["item2"]}>
+          <h2>Popular</h2>
+        </div>
+        <div className={styles["item3"]}>
+          <h2>Airing today</h2>
+        </div>
+        <div className={styles["item4"]}>
+          <h2>On TV</h2>
+        </div>
+        <div className={styles["item5"]}>
+          <h2>Top rated</h2>
+        </div>
+      </div>
     </>
   );
 };
