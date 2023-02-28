4391b074498021918d84b80c72f1f379f7873856
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-02-28 21:30:24 +0000

    Style on movie home page

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -13,27 +13,40 @@ const MovieHome = (props: Props) => {
       <div className={styles["parent"]}>
         <div className={styles["item0"]}>
           <div className={styles["search"]}>
-            <h2>Search now for movies, TV shows or people...</h2>
-            <div className={styles["input-container"]}>
-              <input type="text" />
-              <button>Search</button>
-            </div>
+            <form
+              onSubmit={(e) => {
+                // handleSubmit(e);
+              }}
+            >
+              <div className={styles["input-container"]}>
+                <input
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={searchText}
+                  onChange={(e) => {
+                    newSearch(e.target.value);
+                  }}
+                />
+                <button type="submit">Search</button>
+              </div>
+            </form>
           </div>
         </div>
         <div className={styles["item1"]}>
           <h2>Bullet points</h2>
         </div>
         <div className={styles["item2"]}>
-          <h2>Popular</h2>
+          <h2>Popular movies</h2>
         </div>
         <div className={styles["item3"]}>
-          <h2>Now playing</h2>
+          <h2>Movies now playing</h2>
         </div>
         <div className={styles["item4"]}>
-          <h2>Upcoming</h2>
+          <h2>Upcoming movies</h2>
         </div>
         <div className={styles["item5"]}>
-          <h2>Top rated</h2>
+          <h2>Top rated movies</h2>
         </div>
       </div>
     </>
