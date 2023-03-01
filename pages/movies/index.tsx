1ffbd06ff4b76a8a45d821cb3e50dc5073506292
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-01 22:08:15 +0000

    Add carousel to movie home

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/Header";
-import ImageCard from "@/components/ImageCard";
 import { useSearchText } from "@/context/SearchTextContext";
+import placeholder from "@/public/placeholder.png";
 import styles from "@/styles/MovieHome.module.css";
 import { IMovie, IResponse } from "@/types/movies/types";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { GetStaticProps } from "next";
+import Image, { ImageLoader } from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
 
 interface Props {
   data: IResponse;
@@ -15,6 +18,11 @@ interface Props {
 const MovieHome = ({ data }: Props) => {
   const { searchText, newSearch } = useSearchText();
   const router = useRouter();
+  const myLoader = ({ src }: any) => {
+    return src;
+  };
+
+  const [error, setError] = useState(false);
 
   return (
     <>
@@ -43,22 +51,37 @@ const MovieHome = ({ data }: Props) => {
           </div>
         </div>
         <div className={styles["item1"]}>
-          {data.results.slice(0, 1).map((movie: IMovie) => {
-            return (
-              <ImageCard
-                key={movie.id}
-                title={movie.title}
-                image={`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`}
-                link={`/movies/${movie.id}/`}
-              />
-            );
-          })}
+          <Carousel fade>
+            {data.results.map((movie: IMovie) => {
+              return (
+                <Carousel.Item key={movie.id}>
+                  <Image
+                    loader={myLoader}
+                    src={
+                      !error
+                        ? `https://www.themoviedb.org/t/p/w500/${movie.poster_path}`
+                        : placeholder
+                    }
+                    alt={"hello"}
+                    width={600}
+                    height={1034}
+                    className={"d-block w-100"}
+                    onError={() => {
+                      setError(true);
+                    }}
+                    unoptimized
+                    priority
+                  />
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
         </div>
         <Link href="/movies/popular/" className={styles["item2"]}>
           <h2>Popular movies</h2>
         </Link>
         <Link href="/movies/now-playing/" className={styles["item3"]}>
-          <h2>Movies now plying</h2>
+          <h2>Movies now playing</h2>
         </Link>
         <Link href="/movies/upcoming/" className={styles["item4"]}>
           <h2>Upcoming movies</h2>
