018b1703552654aedc93103fdf38b27fc87831cb
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-02 00:14:15 +0000

    Add carousel to tv home

--- pages/tv-shows/index.tsx
+++ pages/tv-shows/index.tsx
@@ -1,13 +1,18 @@
 import Header from "@/components/Header";
 import { useSearchText } from "@/context/SearchTextContext";
 import styles from "@/styles/TvHome.module.css";
+import { IResponse } from "@/types/tv-shows/types";
+import { CCarousel, CCarouselItem, CImage } from "@coreui/react";
+import { GetStaticProps } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
-type Props = {};
+interface Props {
+  data: IResponse;
+}
 
-const TvShowHome = (props: Props) => {
+const TvShowHome = ({ data }: Props) => {
   const { searchText, newSearch } = useSearchText();
   const router = useRouter();
 
@@ -38,7 +43,21 @@ const TvShowHome = (props: Props) => {
           </div>
         </div>
         <div className={styles["item1"]}>
-          <h2>Bullet points</h2>
+          <div className="local-bootstrap">
+            <CCarousel controls>
+              {data.results.map((movie) => {
+                return (
+                  <CCarouselItem key={movie.id}>
+                    <CImage
+                      className="d-block w-100"
+                      src={`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`}
+                      alt="slide 1"
+                    />
+                  </CCarouselItem>
+                );
+              })}
+            </CCarousel>
+          </div>
         </div>
         <Link href="/tv-shows/popular/" className={styles["item2"]}>
           <h2>Popular</h2>
@@ -59,3 +78,18 @@ const TvShowHome = (props: Props) => {
 };
 
 export default TvShowHome;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const key = process.env.DB_KEY;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`
+  );
+  const data: IResponse = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
