74fa843889147c91b521c393dce39312861416cd
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-02 22:41:14 +0000

    Change padding on carousel

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = [
-  {
-    reactStrictMode: true,
-  },
-  {
-    async redirects() {
-      return [
-        {
-          source: "/movies",
-          destination: "/movies/popular",
-          permanent: true,
-        },
-      ];
-    },
-  },
-];
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
 
 module.exports = nextConfig;
