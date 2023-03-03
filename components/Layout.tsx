3024dc818a17fbd3a5595f46e498009986263aa6
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-03 00:03:03 +0000

    Add new favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,6 +9,24 @@ export default function Layout({ children }: any) {
       <Head>
         <title>Next Movie Website</title>
         <meta name="Layout" content="Next Movie Website" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Navbar />
       <main>{children}</main>
