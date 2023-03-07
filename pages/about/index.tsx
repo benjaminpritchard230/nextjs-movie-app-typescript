e4b1b2deb7f70b41ec5598c4f9ef84b62f3f6d93
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-07 04:10:40 +0000

    Edit footer styling and style about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,16 @@
+import Header from "@/components/Header";
+import styles from "@/styles/About.module.scss";
 import React from "react";
 
 type Props = {};
 
 const AboutPage = (props: Props) => {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <Header text={"About"} />
+      <div className={styles["parent"]}></div>
+    </>
+  );
 };
 
 export default AboutPage;
